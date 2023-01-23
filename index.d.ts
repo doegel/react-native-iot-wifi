b42a41594332b38034c4aff94809b6ba2c66af27
Felix Rudat <frudat@doegel.de>
2023-01-23 15:42:27 +0100

    meta: add new api to types

--- index.d.ts
+++ index.d.ts
@@ -13,6 +13,7 @@ declare module 'react-native-iot-wifi' {
     | [string, boolean, (error: string) => void];
 
   export namespace RNWifi {
+    function requestPermission(): void;
     function isApiAvailable(cb: (available: boolean) => void): void;
     function getSSID(cb: (ssid: string) => void): void;
     function connect(...args: ConnectArgs): void;
